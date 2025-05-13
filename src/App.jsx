6afac37a3c7a1fbdba003fe2479ca5d61b7a1d38
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-05-13 15:16:34 +0200

    ADD : Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Card from './assets/components/Card';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 export default function App() {
 
   const [politici, setPolitici] = useState([]);
+  const [politico, setPolitico] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:5000/politicians')
@@ -13,14 +14,32 @@ export default function App() {
       .catch(error => console.error(error))
   }, [])
 
+
+  const fitroPolitici = useMemo(() => {
+    return politici.filter((p) => {
+      const nome = p.name.toLowerCase().includes(politico.toLowerCase());
+      const biografia = p.biography.toLowerCase().includes(politico.toLowerCase());
+      return nome || biografia
+    })
+  }, [politici, politico])
+
   return (
     <>
-
       <h1>I Politici</h1>
+      <div className='row'>
+        <div className='col-3 m-3'>
+          <input
+            type="text"
+            className='form-control'
+            placeholder='Cerca per nome o biografia'
+            value={politico}
+            onChange={e => setPolitico(e.target.value)} />
+        </div>
+      </div>
       <div className='container'>
         <div className='row'>
           {
-            politici.map((p) => {
+            fitroPolitici.map((p) => {
               return (
                 <Card key={p.id} politico={p} />
               )
